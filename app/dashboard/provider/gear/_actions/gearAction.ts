5337c3cc928b090fd453bb5c9dc7ddbcc422a601
Fahmid-run <fahmiduddinnabil6064@gmail.com>
2026-08-16 14:07:51 +0600

    feat: gear update implement

--- app/dashboard/provider/gear/_actions/gearAction.ts
+++ app/dashboard/provider/gear/_actions/gearAction.ts
@@ -4,8 +4,6 @@ import { cookies } from "next/headers";
 import { gearSchema } from "../_schema/gearschema";
 import path from "path";
 import { mkdir, writeFile } from "fs/promises";
-import { verfiyToken } from "@/app/utils/jwt";
-import { configs } from "@/app/utils/configs";
 
 export const gearAction = async (prevState: any, formData: FormData) => {
   const cookie = cookies();
@@ -61,6 +59,73 @@ export const gearAction = async (prevState: any, formData: FormData) => {
   });
 
   const result = await res.json();
+
+  return result;
+};
+
+export const gearEditAction = async (prevState: any, formData: FormData) => {
+  const cookie = cookies();
+
+  const accessToken = (await cookie).get("accessToken")?.value;
+
+  const gearId = formData.get("gearId") as string;
+
+  const file = formData.get("image") as File | null;
+
+  let imagePath = "";
+
+  if (file && file.size > 0) {
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const uploadDir = path.join(process.cwd(), "public/uploads");
+
+    await mkdir(uploadDir, { recursive: true });
+
+    const uniqueName = `${Date.now()}-${file.name.replace(/\s+/g, "_")}`;
+    const filePath = path.join(uploadDir, uniqueName);
+
+    // 4. Save to disk
+    await writeFile(filePath, buffer);
+
+    // 5. Public URL path to store in database
+    imagePath = `/uploads/${uniqueName}`;
+  }
+
+  const rawData = {
+    ...Object.fromEntries(formData.entries()),
+    image: imagePath,
+  };
+
+  const validated = gearSchema.safeParse(rawData);
+
+  if (!validated.success) {
+    return {
+      success: false,
+      values: rawData,
+      errors: validated.error.flatten().fieldErrors,
+    };
+  }
+
+  const payload = validated.data;
+
+  console.log(payload);
+
+  const res = await fetch(
+    `${process.env.BACKEND_API_URL}/api/provider/gear/${gearId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${accessToken}`,
+      },
+      body: JSON.stringify(payload),
+    },
+  );
+
+  const result = await res.json();
+
+  console.log(result);
 
   return result;
 };
